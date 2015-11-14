57ed418af08b0ec72736d0361e0d11bfe1b62903
Nick Hollon <falling.masonry@gmail.com>
2015-11-13 17:15:17 -0800

    It moves!

--- src/Main.elm
+++ src/Main.elm
@@ -4,22 +4,31 @@ import Array exposing (Array)
 import Color exposing (Color)
 import Graphics.Element as Element
 import Graphics.Collage as Collage
+import Time exposing (Time)
 
 import Interpolate.Bicubic as Bicubic
+import TimeEvolution
+
 
 main : Signal Element.Element
-main = 
-  Signal.foldp update init input |> Signal.map view
-
-
-    
-type alias Model =
-  { terrain : Bicubic.Spline
+main =
+  { deltas = Time.fps 30
+  , env = Signal.constant env
   }
+     |> TimeEvolution.limit (Time.second * 0.25)
+     |> TimeEvolution.evolve laws init
+     |> Signal.map (view env)
 
+
+type alias Env =
+  { terrain : Bicubic.Spline
+  , mass : Float
+  , g : Float
+  }
+               
   
-init : Model
-init =
+env : Env
+env =
   let
     data =
       [ [ 5, 5, 5, 5, 5 ]
@@ -38,30 +47,58 @@ init =
       { x = 200, y = 200 }
   in
     { terrain = Bicubic.withRange start end data
+    , mass = 1
+    , g = 1000
     }
 
 
-
-type Update =
-  Dummy {}
-
-           
-input : Signal Update
-input =
-  Signal.map Dummy (Signal.constant {})
+type alias Model =
+  { position : Vector
+  , momentum : Vector
+  }
 
 
-
-update : Update -> Model -> Model
-update up model =
-  case up of
-    Dummy a ->
-      model
-
+init : Model
+init =
+  { position = { x = 100, y = -100 }
+  , momentum = { x = 0, y = 0 }
+  }
 
 
-view : Model -> Element.Element
-view model =
+laws : TimeEvolution.Laws Model Env
+laws =
+  { add a b =
+      { position = pointMap2 (+) a.position b.position
+      , momentum = pointMap2 (+) a.momentum b.momentum
+      }
+    
+  , scale f a =
+      { position = pointMap ((*) f) a.position
+      , momentum = pointMap ((*) f) a.momentum
+      }
+    
+  , force env model =
+      let
+        gradient =
+          Bicubic.gradientAt model.position env.terrain
+
+        discr =
+          sqrt (1 + gradient.x^2 + gradient.y^2)
+
+        changePos p =
+          p / env.mass / discr
+
+        changeMom s =
+          negate env.mass * env.g * s / discr
+      in
+        { position = pointMap changePos model.momentum
+        , momentum = pointMap changeMom gradient
+        }
+  }
+
+      
+view : Env -> Model -> Element.Element
+view env model =
   let
     size =
       { x = 400, y = 400 }
@@ -71,21 +108,27 @@ view model =
 
     point pos =
       Collage.rect 20 20
-        |> Collage.filled (colorOf pos model.terrain)
+        |> Collage.filled (colorOf pos env.terrain)
         |> Collage.move (pos.x, pos.y)
 
     points =
       gridInit point size resolution
+        |> Collage.group
+
+    ball =
+      Collage.circle 5
+        |> Collage.filled Color.red
+        |> Collage.move (model.position.x, model.position.y)
   in
-    Collage.collage 500 500 points
+    Collage.collage 500 500 [ points, ball ]
 
 
 gridInit : (Vector -> a) -> Vector -> Vector -> List a
 gridInit f size resolution =
   let
     toCoords index =
-      pointMap (\i n -> i/n - 0.5) index resolution
-        |> pointMap (*) size
+      pointMap2 (\i n -> i/n - 0.5) index resolution
+        |> pointMap2 (*) size
       
     item j i =
       f (toCoords { x = toFloat i, y = toFloat j })
@@ -102,9 +145,17 @@ colorOf v spline =
   Bicubic.valueAt v spline
     |> (\f -> (7 - f) / 7)
     |> Color.grayscale
-        
-pointMap : (a -> b -> c) -> {x:a,y:a} -> {x:b,y:b} -> {x:c,y:c}
-pointMap f a b =
+
+
+pointMap : (a -> b) -> {x:a,y:a} -> {x:b,y:b}
+pointMap f a =
+  { x = f a.x
+  , y = f a.y
+  }
+
+       
+pointMap2 : (a -> b -> c) -> {x:a,y:a} -> {x:b,y:b} -> {x:c,y:c}
+pointMap2 f a b =
   { x = f a.x b.x
   , y = f a.y b.y
   }
