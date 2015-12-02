4302fa485e21f95e08835e9cd0cc24edc0dc1561
Nick Hollon <falling.masonry@gmail.com>
2015-12-02 15:27:45 -0800

    Show hand and book on start screen

--- src/Menu.elm
+++ src/Menu.elm
@@ -2,7 +2,7 @@ module Menu (engine, view) where
 
 import Graphics.Element as Element exposing (Element)
 
-import Math.Vector2 as Vec2
+import Math.Vector2 as Vec2 exposing (Vec2)
 import Collision2D
 
 import Types exposing (..)
@@ -27,24 +27,76 @@ engine =
       if data.continue then Just Ready else Nothing
   }
 
+
 view : Data -> Element
 view data =
   let
-    bookHull =
-      [ Vec2.vec2 0 140
-      , Vec2.vec2 500 140
-      , Vec2.vec2 500 360
-      , Vec2.vec2 0 360
-      ] |> Collision2D.fromVectors
-
-    cursor =
-      Vec2.vec2 (toFloat (fst data.cursor)) (toFloat (snd data.cursor))
-                              
-    imageSource =
-      if Collision2D.isInside cursor bookHull then
-        "/img/open_book.svg"
-      else
-        "/img/closed_book.svg"
+    position (x, y) =
+      Element.topLeftAt (Element.absolute x) (Element.absolute y)
+             
+    placeAt pt img =
+      Element.container 1000 800 (position pt) img
   in
-    Element.image 470 220 imageSource
-      |> Element.container 500 500 Element.middle 
+    [ bookIcon, handIcon ]
+    |> List.map (drawIcon data.cursor)
+    |> List.map2 placeAt [(0, 100), (500, 100)]
+    |> Element.flow Element.outward
+
+
+bookIcon : Icon
+bookIcon =
+  let
+    hull =
+      [ Vec2.vec2 0 100
+      , Vec2.vec2 470 100
+      , Vec2.vec2 470 320
+      , Vec2.vec2 0 320
+      ]
+  in
+    icon hull 470 220 "open_book.svg" "closed_book.svg"
+
+  
+handIcon : Icon
+handIcon =
+  let
+    hull =
+      [ Vec2.vec2 500 100
+      , Vec2.vec2 737 100
+      , Vec2.vec2 737 340
+      , Vec2.vec2 500 340
+      ]
+  in
+    icon hull 237 240 "open_hand.svg" "resting_fist.svg"
+
+      
+icon : List Vec2 -> Int -> Int -> String -> String -> Icon
+icon hull width height focus blur =
+  { hull = Collision2D.fromVectors hull
+  , focusImage = image width height focus
+  , blurImage = image width height blur
+  }
+
+  
+drawIcon : (Int,Int) -> Icon -> Element
+drawIcon (mouseX, mouseY) icon =
+  let
+    cursor =
+      Vec2.vec2 (toFloat mouseX) (toFloat mouseY)
+  in          
+    if Collision2D.isInside cursor icon.hull then
+      icon.focusImage
+    else
+      icon.blurImage
+
+
+image : Int -> Int -> String -> Element
+image width height filename =
+  "/img/" ++ filename
+    |> Element.image width height
+       
+         
+type alias Icon =
+  { hull : Collision2D.Hull
+  , focusImage : Element
+  , blurImage : Element
+  }
